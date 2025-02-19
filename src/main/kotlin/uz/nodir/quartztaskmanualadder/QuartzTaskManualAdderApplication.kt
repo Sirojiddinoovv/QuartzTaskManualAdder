6cb76d952c8cd85a7709a1529436a60ec39ebbc0
Nodir <davrbankit@gmail.com>
2025-02-19 09:09:29 +0500

    first commit;

---
+++ src/main/kotlin/uz/nodir/quartztaskmanualadder/QuartzTaskManualAdderApplication.kt
@@ -0,0 +1,11 @@
+package uz.nodir.quartztaskmanualadder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuartzTaskManualAdderApplication
+
+fun main(args: Array<String>) {
+    runApplication<QuartzTaskManualAdderApplication>(*args)
+}
